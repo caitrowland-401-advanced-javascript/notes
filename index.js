e77ca5fed51d140694230313d12eb728a5e75eca
caitleigh <caitleigh27@gmail.com>
2020-02-10 20:18:16 -0800

    updated index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Input = require('./lib/input.js');
 const Note= require('./lib/notes.js');
 
 const options = new Input();
-options.valid() ? Note.add(options) : help();
+options.valid() ? Note.fetch(options) : help();
 
 function help() {
     console.log('heeeelp meeeeee');
